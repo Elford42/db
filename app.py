4c5f167f229f009f1d42b03406855697c4bec362
meguelyousif <meguel.yousif@ec.gc.ca>
2024-08-26 12:00:35 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,147 @@
+from dash import Dash, html, dcc 
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+from sqlalchemy import create_engine
+from credentials import sql_engine_string_generator
+import os
+from dotenv import load_dotenv 
+
+# initialize the dash app as 'app'
+app = Dash(__name__,external_stylesheets=[dbc.themes.SLATE])
+
+# conect to swapit and dcp databases
+sql_engine_string=sql_engine_string_generator('DATAHUB_PSQL_SERVER','DATAHUB_SWAPIT_DBNAME','DATAHUB_PSQL_USER','DATAHUB_PSQL_PASSWORD')
+swapit_sql_engine=create_engine(sql_engine_string)
+
+sql_engine_string=sql_engine_string_generator('DATAHUB_PSQL_SERVER','DATAHUB_DCP_DBNAME','DATAHUB_PSQL_USER','DATAHUB_PSQL_PASSWORD')
+dcp_sql_engine=create_engine(sql_engine_string)
+
+
+# set up the app layout
+## MOBILE
+app.layout = html.Div(
+    children=[
+        
+        #title + instructions
+        html.H1('SWAPIT CRUISER Field Log'),
+        html.Div([
+            html.Span('Required fields indicated by '),
+            html.Span('*',style={"color": "red","font-weight": "bold"})
+        ]),
+        html.Br(),
+        
+        # Name
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2([
+                    "Name",
+                    html.Span('*',style={"color": "red","font-weight": "bold"})
+                ])),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Site
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2([
+                    "Site",
+                    html.Span('*',style={"color": "red","font-weight": "bold"})
+                ])),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Instrument
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2([
+                    "Instrument",
+                    html.Span('*',style={"color": "red","font-weight": "bold"})
+                ])),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Flag Category
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2("Flag Category")),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Flag
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2("Flag")),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Note
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2("Note")),
+                dbc.Input(placeholder="...", type="text"),
+                html.Br()],
+                width = 6
+            )],
+            justify = "center"
+        ),
+        
+        # Date and time
+        dbc.Row([
+            dbc.Col(
+                [dbc.Label(html.H2([
+                    "Datetime",
+                    html.Span('*',style={"color": "red","font-weight": "bold"})
+                ])),
+                dbc.Input(placeholder="...", type="datetime-local",step="1")],
+                width = 8
+            )],
+            justify = "center"
+        ),
+        
+        dbc.Row([
+            dbc.Col([
+                dbc.Input(placeholder="Timezone", type="text"),
+                
+                html.Br()],
+                width = 4
+            )],
+            justify = "center"
+        )
+        
+    
+    ],
+    style={'textAlign': 'center'}
+)
+
+
+
+@app.callback(
+    Output('', ''))
+
+def update_output(start_date, end_date):
+    pass
+    
+if __name__=='__main__':
+    app.run_server(debug=True, use_reloader=False,port=8050)
---
+++ credentials.py
@@ -0,0 +1,49 @@
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+import os
+from dotenv import load_dotenv 
+
+def sql_engine_string_generator(datahub_host, datahub_db, datahub_user, datahub_pwd): 
+
+    # set a try except clause to grab the online credentials keys and if not, grab them locally as environment variables
+    try:
+        # set the key vault path
+        KEY_VAULT_URL = "https://fsdh-swapit-dw1-poc-kv.vault.azure.net/"
+        error_occur = False
+
+        # Retrieve the secrets containing DB connection details
+        credential = DefaultAzureCredential()
+        secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+
+        # Retrieve the secrets containing DB connection details
+        DB_HOST = secret_client.get_secret(datahub_host).value
+        DB_NAME = secret_client.get_secret(datahub_db).value
+        DB_USER = secret_client.get_secret(datahub_user).value
+        DB_PASS = secret_client.get_secret(datahub_pwd).value
+        print ('Credentials loaded from FSDH')
+
+    except Exception as e:
+        # declare FSDH keys exception
+        error_occur = True
+        # print(f"An error occurred: {e}")
+
+        # load the .env file using the dotenv module remove this when running a powershell script to confirue system environment vars
+        load_dotenv() # default is relative local directory 
+        env_path='.env'
+        DB_HOST = os.getenv(datahub_host)
+        DB_NAME = os.getenv(datahub_db)
+        DB_USER = os.getenv(datahub_user)
+        DB_PASS = os.getenv(datahub_pwd)
+        print ('Credentials loaded locally')
+
+    # set the sql engine string
+    sql_engine_string=('postgresql://{}:{}@{}/{}?sslmode=require').format(DB_USER,DB_PASS,DB_HOST,DB_NAME)
+    print ('sql engine string: ',sql_engine_string)
+    return sql_engine_string
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
