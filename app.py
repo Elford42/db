1ee226bd621608712a9defcaed0c41d2068b5eb8
meguelyousif <meguel.yousif@ec.gc.ca>
2024-11-20 21:35:42 -0500

    attempt at fix

--- app.py
+++ app.py
@@ -446,4 +446,4 @@ def before_request():
 #%% Run The app
 #server = app.server 
 if __name__=='__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
+    app.run_server(debug=True, host='0.0.0.0', port=80)
